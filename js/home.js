f8e5e974cd043053b2bdf926e65629178f10b09c
Chen12581 <446354153@qq.com>
2019-06-15 15:52:02 +0800

    six

---
+++ js/home.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    var swiper = new Swiper('.swiper-container', {
+        //循坏轮播图
+        loop: true,
+        //是否自动播放默认false
+        autoplay: {
+            //触摸后是否停止自动滑动，默认true
+            disableOnInteraction: false,
+        },
+        //分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+    wrap.init();
+
+})
+
+var wrap = {
+    init: function() {
+        this.mbList();
+    },
+    mbList: function() {
+        $(".headerRightImg").on("click", function() {
+            $(".headerRightImg img").each(function(i) {
+                if (i == 0 && $(this).is(":hidden")) {
+                    $(".mbList").animate({
+                        left: '150%'
+                    })
+                } else if (i == 1 && $(this).is(":hidden")) {
+                    $(".mbList").animate({
+                        left: '0'
+                    })
+                }
+                $(this).toggle();
+            })
+        })
+    }
+}
