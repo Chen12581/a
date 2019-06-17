4fb74ccb03dfdbf1134021b283618e9469bb832b
Chen12581 <446354153@qq.com>
2019-06-17 16:09:39 +0800

    update

--- js/home.js
+++ js/home.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
     wrap.init();
-
 })
-
 var wrap = {
     init: function() {
         this.mbList();
         this.banner();
+
     },
     mbList: function() {
         $(".headerRightImg").on("click", function() {
@@ -39,5 +38,33 @@ var wrap = {
                 el: '.swiper-pagination',
             },
         });
+    },
+    addLink: function(blogName) {
+        var body_element = document.body;
+        var selection;
+        selection = window.getSelection() ? window.getSelection() : document.selection.createRange().text;
+        if (window.clipboardData) { // Internet Explorer
+            var pagelink = "\r\n\r\n 原文出自" + blogName + " 转载请保留原文链接: " + document.location.href + "";
+            var copyText = selection + pagelink;
+            window.clipboardData.setData("Text", copyText);
+            return false;
+        } else {
+            var pagelink = " 原文出自" + blogName + " 转载请保留原文链接: " + document.location.href + "";
+            var copyText = selection + pagelink;
+            var newDiv = document.createElement('div');
+            newDiv.style.position = 'absolute';
+            newDiv.style.left = '-99999px';
+            body_element.appendChild(newDiv);
+            newDiv.innerHTML = copyText;
+            selection.selectAllChildren(newDiv);
+            window.setTimeout(function() {
+                body_element.removeChild(newDiv);
+            }, 0);
+        }
+    }
+}
+document.oncopy = function() {
+    if ($('body')) {
+        wrap.addLink('花园竹木官网');
     }
 }
